5f831bfb7b8e28ebdabf64966d16e27c42a0e024
Eugene Shubin <eugene.shubin@sumup.com>
2015-02-05 18:27:55 +0100

    reproduce a bug in poolboy

--- src/poolboy_test.erl
+++ src/poolboy_test.erl
@@ -2,25 +2,39 @@
 
 
 %% API
--export([stop_normal/0, stop_error/0]).
+-export([stop_normal/0, stop_error/0, stop_normal/1, stop_error/1]).
 
 -include("poolboy_test.hrl").
 
+-define(DEFAULT_TIMEOUT, 5000).
+
+stop_normal(Timeout) ->
+    execute_on_worker(fun worker:stop_normal/1, Timeout).
+
 stop_normal() ->
-    poolboy:transaction(
-        ?POOL_NAME,
-        fun(Worker) ->
-            worker:stop_normal(Worker)
-        end
-    ).
+    stop_normal(?DEFAULT_TIMEOUT).
+
+stop_error(Timeout) ->
+    execute_on_worker(fun worker:stop_error/1, Timeout).
 
 stop_error() ->
-    poolboy:transaction(
-        ?POOL_NAME,
-        fun(Worker) ->
-            worker:stop_error(Worker)
-        end
-    ).
+    stop_error(?DEFAULT_TIMEOUT).
+
+
+
+execute_on_worker(Fun, Timeout) ->
+    try
+        poolboy:transaction(
+            ?POOL_NAME,
+            fun(Worker) ->
+                Fun(Worker)
+            end,
+            Timeout
+        )
+        catch
+            exit:{timeout,_} ->
+                timeout
+    end.
 
 -include_lib("eunit/include/eunit.hrl").
 
@@ -38,6 +52,21 @@ basic_test_() ->
             fun test_double_error/0
         ]
     }.
+
+failing_test_() ->
+    {
+        setup,
+        fun setup/0,
+        fun teardown/1,
+        [
+            ?_assertEqual({ready,1,0,0}, poolboy:status(?POOL_NAME)),
+            ?_assertEqual(timeout, stop_normal(0)),
+            ?_assertEqual({full,0,0,1}, poolboy:status(?POOL_NAME)), %% WHY?
+            ?_assertEqual(timeout, stop_normal(4000))  %% After timeout in poolboy:transaction every call times out.
+        ]
+    }.
+
+
 
 test_double_normal() ->
     ?assertEqual(ok, stop_normal()),
